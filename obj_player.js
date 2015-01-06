3380db01519985c2d397eea3d53dd3e71362b518
Benjamin Anderson <Benjamin.k.anderson@gmail.com>
2015-01-06 12:33:26 -0700

    Not sure what this change even is anymore hahaha.

--- obj_player.js
+++ obj_player.js
@@ -3,18 +3,16 @@ object_create( "obj_player" );
 
 // Create Event
 obj_player.create_event = function() {
-	var x = this.x;
 	console.log( "Create Event" );
-	console.log( x );
 }
 
 // Step Event
 obj_player.step_event = function() {
 	if ( keyboard_check( vk_right ) ) {
-		console.log( "Right key" );
+		x++;
 	}
 
-	if ( keyboard_check( vk_left ) ) {s
-		console.log( "Left key" );
+	if ( keyboard_check( vk_left ) ) {
+		x--;
 	}
 }
