e3a81ac4a6aa8d2f96305d44b658a17f7769ae2b
Benjamin Anderson <Benjamin.k.anderson@gmail.com>
2015-01-05 21:10:09 -0700

    Started the js file that will work as a gml interpreter.

---
+++ gmljs.js
@@ -0,0 +1,56 @@
+// This is just a test javascript file I'm using to mess around with a
+// javascript gml interpreter
+
+objects = [];
+
+// Object creation function
+function object_create( name ) {
+    window[ name ] = function() {
+        this.x = 0;
+        this.y = 0;
+    };
+    
+    window[ name ].create_event = function() {};
+    window[ name ].step_event = function() {};
+}
+
+//script creation function
+function script_create( name, func ) {
+    window[ name ] = func;
+}
+
+function instance_create( x, y, object ) {
+    var obj = new object();
+    obj.create_event = object.create_event;
+    obj.create_event.call( obj );
+    return obj;
+}
+
+function handleCreateEvents() {
+    // Handle the create event
+    for (var i=0; i<objects.length; i++) {
+        var obj = objects[ i ];
+        if ( obj.create_event ) {
+            obj.create_event.call( obj );
+        }
+    }
+}
+
+function handleOngoingEvents() {
+    // Handle the step event
+    for (var i=0; i<objects.length; i++) {
+        var obj = objects[ i ];
+        if ( obj.step_event ) {
+            obj.step_event.call( obj );
+        }
+    }
+}
+
+function gameLoop() {
+    handleOngoingEvents();
+}
+
+handleCreateEvents();
+function run( fps ) {
+    setInterval( gameLoop, 1000/fps );
+}
